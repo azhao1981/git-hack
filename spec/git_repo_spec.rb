fa94255e4c9f8577e538083462a46e7bad8a7a8d
weizhao <azhao.1981@gmail.com>
2012-02-22 23:21:25 +0800

    test commit

--- spec/git_repo_spec.rb
+++ spec/git_repo_spec.rb
@@ -44,7 +44,7 @@ describe GitRepo do
 			it "Should be success " do
         		#`cat "hello" >> "#{project_dir}/spec/tmp/hello_for_rspec"`
 				`cat "hello"`
-        		`cat "#{project_dir}"`
+        		`echo "#{project_dir}"`
 				@gitrepo.git_save(@msg).should be_success
 			end
 			after { del_dir(@dir) }
